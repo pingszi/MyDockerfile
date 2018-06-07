e18fb53d3619a6d005f1650aea889de140735709
Pings <275598139@qq.com>
2018-06-07 16:04:40 +0800

    mysql_ip

--- aiqa/conf/start_aiqa.sh
+++ aiqa/conf/start_aiqa.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# 根据传入的MYSQL_IP环境变量修改项目的mysql地址
+mysqlip=$MYSQL_IP
+if [ $mysqlip ]; then
+   sed -i 's/192.168.180.30/'${mysqlip}'/g' /opt/project/product/AIQA/AIQA/settings.py
+fi
+
 nginx
 uwsgi --ini /opt/project/product/script/uwsgi.ini
 /opt/project/redis-4.0.9/src/redis-server /opt/project/redis-4.0.9/redis.conf
