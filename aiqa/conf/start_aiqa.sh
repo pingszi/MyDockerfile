fdcb8b0e34fdf77fc2e85fcbf85a7e12555625b2
Pings <275598139@qq.com>
2018-09-21 15:04:08 +0800

    pro

--- aiqa/conf/start_aiqa.sh
+++ aiqa/conf/start_aiqa.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 # 根据传入的MYSQL_IP环境变量修改项目的mysql地址
 mysqlip=$MYSQL_IP
+ipreg='[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'
+
 if [ $mysqlip ]; then
-   sed -i 's/192.168.180.30/'${mysqlip}'/g' /opt/project/product/AIQA/AIQA/settings.py
+   #sed -i 's/192.168.180.30/'${mysqlip}'/g' /opt/project/product/AIQA/AIQA/settings.py
+   sed -i "s/^'HOST': '${ipreg}',$/'HOST': '${mysqlip}',/" /opt/project/product/AIQA/AIQA/settings.py	
 fi
 
 nginx
